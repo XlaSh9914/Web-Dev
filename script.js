00f6fbab7174db235548fed47d745accf99c67cd
XlaSh9914 <62474668+XlaSh9914@users.noreply.github.com>
2024-12-08 22:11:15 +0530

    bug fix
    cart dish removal and cart button logo bug fix

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for (let i = 0; i < dishes.length; i++) {
   dishCard.className = "dish-card";
   dishCard.innerHTML = ` <img src=${dishes[i].imageURL} alt="Dish Image" class="dish-image">
   <div class="dish-details">
-    <h3 class="dish-name">${dishes[i].name}</h3>
+    <h3 class="dish-name concert-one-title">${dishes[i].name}</h3>
     <p class="dish-description">${dishes[i].description}</p>
     <div class="price-quantity">
       <span class="dish-price">$${dishes[i].price}</span>
@@ -20,22 +20,30 @@ for (let i = 0; i < dishes.length; i++) {
 
 const cartItem = document.querySelector("#cart-items");
 const cartSlider = document.getElementById("cart-slider");
+const cartToggleArrow = document.getElementById("cart-toggle")
 
 document.getElementById("cart-toggle").addEventListener("click", () => {
-  if (cartSlider.style.maxHeight != "400px" && cartItem.innerHTML != "")
+  if (cartSlider.style.maxHeight != "400px" && cartItem.innerHTML != ""){
     cartSlider.style.maxHeight = "400px";
-  else cartSlider.style.maxHeight = "28px";
+    cartToggleArrow.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#e8eaed"><path d="M480-360 280-560h400L480-360Z"/></svg>';
+  }
+  else{
+    cartToggleArrow.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#e8eaed"><path d="m280-400 200-200 200 200H280Z"/></svg>';
+    cartSlider.style.maxHeight = "28px";
+  } 
+    
 });
 
 function getDishById(id) {
   return dishes.find((dish) => dish.id === parseInt(id));
 }
 
-function removeCartItem(id){
-  
+function removeFromCart(dishId){
+  cart = cart.filter(item => item.id !== dishId);
 }
 
 function updateCartItem(item, subtract = false) {
+  console.log("Before: ",cart);
   const cartTotal = document.getElementById("cart-total");
   const cartItemCount = document.getElementById("cart-count");
   
@@ -52,6 +60,8 @@ function updateCartItem(item, subtract = false) {
       
       cartItemCount.innerHTML = itemCount; 
       cartItem.remove();
+      removeFromCart(item.id);
+      console.log("After: ",cart);
       
       return
     }
@@ -71,6 +81,7 @@ function updateCartItem(item, subtract = false) {
   item.currentPrice = item.price * item.quantity;
   dishPriceElement.innerHTML = "$" + item.currentPrice.toFixed(2);
   dishQuantityElement.innerHTML = item.quantity;
+  console.log("After: ",cart);
 }
 
 let cart = [];
